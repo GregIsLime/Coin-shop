3c8b4f0e9638f030f336633e8275ac0f91ec3b82
Greg <specture9@gmail.com>
2024-11-26 21:56:15 +0300

    qwerty

--- script.js
+++ script.js
@@ -1,47 +1,72 @@
 $('document').ready(function () {
+
     loadGoods()
+    cardButton()
+    loadFilterButtons()
 }
 )
+function refreshCards() {
+    console.log("qwqwqw")
+    let tbody = document.getElementsByTagName("tbody")
+    tbody[0].innerHTML = ''
+    let LS = JSON.parse(localStorage.getItem('card'))
+    let orderList = ""
+    let sum = 0
+    let totalNumbers = 0
+    $.getJSON("products.json", function (data) {
+        let correctedLS = checkingAnOrderList(LS)
+        let y = 0
+        console.log(data)
+        for (let key1 in data) {
+            for (let i = 0; i < correctedLS.length; i++) {
+                if (correctedLS[i][0] == data[key1]['art']) {
+                    orderList = `<tr>
+                    <th scope="row">${y + 1}</th>
+                    <td class="artOfCategory" style="display:none;">${data[key1]['art']}</td>
+                    <td>${data[key1]['nominal']} ${data[key1]['year']}г. ${data[key1]['event']} </td>
+                    <td>
+                    <img class="imgSym minus2" src="pic/minus.png" alt="">
+                    ${correctedLS[i][1]} шт.
+                    <img class="imgSym pluse2" src="pic/more.png" alt="">
+                    </td>
+                    <td>${Number(data[key1]['price']) * Number(correctedLS[i][1])} ₽</td>
+                  </tr>`
+                    tbody[0].insertAdjacentHTML('beforeend', orderList)
+                    orderList = ""
+                    y++
+                    totalNumbers += Number(correctedLS[i][1])
+                    sum += Number(data[key1]['price']) * Number(correctedLS[i][1])
+
+                    const pluse2 = document.querySelectorAll(".pluse2")
+                    pluse2.forEach((element) => {
+                        element.addEventListener("click", pluseAction)
+                    })
+
+                    const minus2 = document.querySelectorAll(".minus2")
+                    minus2.forEach((element) => {
+                        element.addEventListener("click", minusAction)
+                    }
+                    )
+                }
+            }
+        }
+        orderList =
+            `<tr class="totalCalc">
+                <th scope="row">ИТОГО:</th>
+                <td></td>
+                <td>${totalNumbers} шт.</td>
+                <td class="totalCalc">${sum} ₽</td>
+            </tr>`
+        tbody[0].insertAdjacentHTML('beforeend', orderList)
+        orderList = ""
+    })
+}
 
 function cardButton() {
     let cardButton = document.getElementById("cardOfBuyer")
     cardButton.addEventListener("click", () => {
-        let tbody = document.getElementsByTagName("tbody")
-        tbody[0].innerHTML = ''
-        let LS = JSON.parse(localStorage.getItem('card'))
-        let orderList = ""
-        let sum = 0
-        let totalNumbers = 0
-        $.getJSON("products.json", function (data) {
-            let correctedLS = checkingAnOrderList(LS)
-            let y = 0
-            for (let key1 in data) {
-                for (let i = 0; i < correctedLS.length; i++) {
-                    if (correctedLS[i][0] == data[key1]['art']) {
-                        orderList = `<tr>
-                        <th scope="row">${y + 1}</th>
-                        <td>${data[key1]['nominal']} ${data[key1]['event']} </td>
-                        <td>${correctedLS[i][1]} шт.</td>
-                        <td>${Number(data[key1]['price']) * Number(correctedLS[i][1])} ₽</td>
-                      </tr>`
-                        tbody[0].insertAdjacentHTML('beforeend', orderList)
-                        orderList = ""
-                        y++
-                        totalNumbers += Number(correctedLS[i][1])
-                        sum += Number(data[key1]['price']) * Number(correctedLS[i][1])
-                    }
-                }
-            }
-            orderList =
-                `<tr class="totalCalc">
-                    <th scope="row">ИТОГО:</th>
-                    <td></td>
-                    <td>${totalNumbers} шт.</td>
-                    <td class="totalCalc">${sum} ₽</td>
-                </tr>`
-            tbody[0].insertAdjacentHTML('beforeend', orderList)
-            orderList = ""
-        })
+        console.log("123")
+        refreshCards()
     })
 }
 
@@ -65,9 +90,6 @@ function checkingAnOrderList(orderList) {
     return order
 }
 
-cardButton()
-
-
 function filtringCatigories() {
     $.getJSON("products.json", function (data) {
         let = new Array()
@@ -82,7 +104,6 @@ function filtringCatigories() {
     })
 }
 
-
 let isOnlyCoins = false
 let isOnlyMedals = false
 let filterByCountry = "all"
@@ -91,20 +112,19 @@ let filterByCountry = "all"
 // geting JSON file put in HTML
 function loadGoods() {
     $.getJSON("products.json", function (data) {
-
         let filterSet = (isOnlyCoins) ? "монета" : ((isOnlyMedals) ? "медаль" : "none")
         let out = ""
         let isIn = false
-
         for (let key in data) {
             if (data[key]['type'] === filterSet || filterSet === "none") {
                 if (data[key]['country'] === filterByCountry || filterByCountry === "all") {
                     out += `<div class="col h-100">
-        <div class="card">
+        <div id="card-id-${data[key]['art']}" class="card">
           <img src=" ${data[key]['img']}" class="card-img-top w-100 cardimage" alt="...">
-          <div class="card-body ">
+          <div class="card-body } ">
             <h6 class="w-100 card-title text-center fs-5 nominals">${data[key]['nominal']} ${data[key]['year']}г.</h6>
             <h6 class="w-100 card-title text-center fs-5 nominals-comnt">${data[key]['event']}</h6>
+            <p class="w-100 card-text lh-sm fs-6 country">Страна: ${data[key]['country']} </p>
             <p class="w-100 card-text lh-sm fs-6 diam">Диаметр: ${data[key]['diameter']}мм </p>
             <p class="w-100 card-text lh-sm fs-8 mass">Вес: ${data[key]['weight']}гр </p>
             <p class="w-100 card-text lh-sm fs-6 text-center discont">${Math.ceil(data[key]['price'] * 1.6)} ₽</p>
@@ -168,37 +188,75 @@ function addToCard() {
     document.querySelector(`[data-art-button= "${articl}"]`).style.display = "flex"
     this.parentNode.querySelector(".p-card").innerHTML = checking(articl)[0]
     cardIsActive()
-
 }
 
 function pluseAction() {
     let card = JSON.parse(localStorage.getItem('card')) || [];
-    let articl = this.parentNode.getAttribute("data-art-button")
-    card.push(articl)
-    localStorage.setItem("card", JSON.stringify(card))
-    this.parentNode.querySelector(".p-card").innerHTML = checking(articl)[0]
-    cardIsActive()
+    if (this.className === "imgSym pluse") {
+        let articl = this.parentNode.getAttribute("data-art-button")
+        card.push(articl)
+        localStorage.setItem("card", JSON.stringify(card))
+        this.parentNode.querySelector(".p-card").innerHTML = checking(articl)[0]
+        cardIsActive()
+
+    } else if (this.className === "imgSym pluse2") {
+        let nameOfCategory = this.parentNode.parentNode.getElementsByClassName("artOfCategory")
+        let art = nameOfCategory[0].innerText
+        card.push(art)
+        localStorage.setItem("card", JSON.stringify(card))
+        let rightCard = document.getElementById(`card-id-${art}`)
+        let numer = rightCard.querySelector(".p-card").innerHTML
+        rightCard.querySelector(".p-card").innerHTML = parseInt(numer) + 1
+        refreshCards()
+        cardIsActive()
+
+    }
 }
 
 function minusAction() {
     let card = JSON.parse(localStorage.getItem('card')) || [];
-    let articl = this.parentNode.getAttribute("data-art-button")
-    let i = 0
-    for (let el of card) {
-        if (el === articl) {
-            card.splice(i, 1)
-            break
+    if (this.className === "imgSym minus") {
+        let articl = this.parentNode.getAttribute("data-art-button")
+        let i = 0
+        for (let el of card) {
+            if (el === articl) {
+                card.splice(i, 1)
+                break
+            }
+            i++
         }
-        i++
-    }
-    localStorage.setItem("card", JSON.stringify(card))
-    this.parentNode.querySelector(".p-card").innerHTML = checking(articl)[0]
-    if (checking(articl)[0] === 0) {
-        document.querySelector(`[data-art= "${articl}"]`).style.display = ""
-        document.querySelector(`[data-art-button= "${articl}"]`).style.display = "none"
+        localStorage.setItem("card", JSON.stringify(card))
+        this.parentNode.querySelector(".p-card").innerHTML = checking(articl)[0]
+        if (checking(articl)[0] === 0) {
+            document.querySelector(`[data-art= "${articl}"]`).style.display = ""
+            document.querySelector(`[data-art-button= "${articl}"]`).style.display = "none"
+            cardIsActive()
+        }
+        cardIsActive()
+    } else if (this.className === "imgSym minus2") {
+        let nameOfCategory = this.parentNode.parentNode.getElementsByClassName("artOfCategory")
+        let art = nameOfCategory[0].innerText
+        let i = 0
+        for (let el of card) {
+            if (el === art) {
+                card.splice(i, 1)
+                break
+            }
+            i++
+        }
+        localStorage.setItem("card", JSON.stringify(card))
+        let rightCard = document.getElementById(`card-id-${art}`)
+        let numer = rightCard.querySelector(".p-card").innerHTML
+        rightCard.querySelector(".p-card").innerHTML = parseInt(numer) - 1
+        if (rightCard.querySelector(".p-card").innerHTML === "0") {
+            document.querySelector(`[data-art-button= "${art}"]`).style.display = "none"
+            document.querySelector(`[data-art= "${art}"]`).style.display = "flex"
+        }
+        refreshCards()
         cardIsActive()
     }
-    cardIsActive()
+
+
 }
 
 function checking(art) {
@@ -219,43 +277,42 @@ function cardIsActive() {
     let cardMenu = document.querySelector(".cardOfBuyer")
     if (card.length != 0) {
         cardMenu.classList.remove("disabled")
-        cardMenu.innerHTML = `Корзина ${card.length}`
+        cardMenu.innerHTML = `Корзина <span id="card-numberOfProducts">${card.length}</span>`
     } else {
         cardMenu.innerHTML = `Корзина`
         document.querySelector(".cardOfBuyer").classList.add("disabled")
     }
 }
 
-
-let btnOnlyCoins = document.getElementById("btncheck1")
-let btnOnlyMedals = document.getElementById("btncheck2")
-btnOnlyCoins.addEventListener("click", () => {
-    btnOnlyCoins.checked = (btnOnlyCoins.checked) ? true : false
-    btnOnlyMedals.checked = false
-    isOnlyMedals = false
-    isOnlyCoins = (isOnlyCoins) ? false : true
-    document.getElementById("cardKeeper").innerHTML = ''
-    loadGoods()
-
-
-})
-btnOnlyMedals.addEventListener("click", () => {
-    btnOnlyMedals.checked = (btnOnlyMedals.checked) ? true : false
-    btnOnlyCoins.checked = false
-    isOnlyCoins = false
-    isOnlyMedals = (isOnlyMedals) ? false : true
-    document.getElementById("cardKeeper").innerHTML = ''
-    loadGoods()
-})
+function loadFilterButtons() {
+    let btnOnlyCoins = document.getElementById("btncheck1")
+    let btnOnlyMedals = document.getElementById("btncheck2")
+    btnOnlyCoins.addEventListener("click", () => {
+        btnOnlyCoins.checked = (btnOnlyCoins.checked) ? true : false
+        btnOnlyMedals.checked = false
+        isOnlyMedals = false
+        isOnlyCoins = (isOnlyCoins) ? false : true
+        document.getElementById("cardKeeper").innerHTML = ''
+        loadGoods()
 
 
-
-let categoryArray = document.getElementsByClassName("dropdown-item-countrys")
-Array.from(categoryArray).forEach((element) => {
-    element.addEventListener("click", () => {
-        btnGroupDrop1.innerText = element.innerText
-        filterByCountry = element.id
+    })
+    btnOnlyMedals.addEventListener("click", () => {
+        btnOnlyMedals.checked = (btnOnlyMedals.checked) ? true : false
+        btnOnlyCoins.checked = false
+        isOnlyCoins = false
+        isOnlyMedals = (isOnlyMedals) ? false : true
         document.getElementById("cardKeeper").innerHTML = ''
         loadGoods()
     })
-})
+
+    let categoryArray = document.getElementsByClassName("dropdown-item-countrys")
+    Array.from(categoryArray).forEach((element) => {
+        element.addEventListener("click", () => {
+            btnGroupDrop1.innerText = element.innerText
+            filterByCountry = element.id
+            document.getElementById("cardKeeper").innerHTML = ''
+            loadGoods()
+        })
+    })
+}
